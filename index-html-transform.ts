29ad461af42821e5f34e486bbd4eaea9e6b43193
tuan-relipasoft <tuan@relipasoft.com>
2023-01-03 15:42:01 +0700

    lamln - commit

--- index-html-transform.ts
+++ index-html-transform.ts
@@ -3,8 +3,7 @@ import { TargetOptions } from '@angular-builders/custom-webpack';
 export default (targetOptions: TargetOptions, indexHtml: string) => {
   const i = indexHtml.indexOf('</body>');
   const config = `<p>Configuration: ${targetOptions.configuration}</p>`;
-  const demo = '<script src="lamln.js"></script>';
   return `${indexHtml.slice(0, i)}
             ${config}
-            ${indexHtml.slice(i)}`.replace('</body>', `${demo}</body>`);
+            ${indexHtml.slice(i)}`;
 };
